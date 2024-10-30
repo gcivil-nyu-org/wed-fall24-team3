36e3171e7bb8ad024f54d3abad07db95851108d7
Suprateek Chatterjee <suprateek1912@gmail.com>
2024-10-30 13:36:41 -0400

    changed django.config

--- eventsphere/events/models.py
+++ eventsphere/events/models.py
@@ -16,20 +16,6 @@ class CreatorProfile(models.Model):
         return self.creator.username
 
 
-# Create your models here.
-# class Event(models.Model):
-#     name = models.CharField(max_length=200)
-#     location = models.CharField(max_length=200)
-#     date_time = models.DateTimeField()
-#     schedule = models.TextField()
-#     speakers = models.CharField(max_length=500)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-
-#     def __str__(self):
-#         return self.name
-
-
 class Event(models.Model):
     name = models.CharField(max_length=200)
     location = models.CharField(max_length=200)  # This will be the address
