cdbc9c4d9d1b87aa213a9765ffd06d46b0ed312c
Suprateek Chatterjee <suprateek1912@gmail.com>
2024-11-12 03:36:43 -0500

    signals fix

--- eventsphere/events/signals.py
+++ eventsphere/events/signals.py
@@ -1,11 +1,9 @@
-# events/signals.py
-
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Event, ChatRoom
 
-
 @receiver(post_save, sender=Event)
 def create_chat_room(sender, instance, created, **kwargs):
-    if created:
+    # Check if the event is newly created and has a valid 'created_by' field
+    if created and instance.created_by:
         ChatRoom.objects.create(event=instance, creator=instance.created_by)
